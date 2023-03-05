b9947527037a161e5ac2536d739338d43248bfe0
i000Tea <107682438+i000Tea@users.noreply.github.com>
2023-03-05 14:31:35 +0800

    旋转
    矫正角色旋转后移动的方向

--- Assets/Scripts/NewRogue/PlayerMove.cs
+++ Assets/Scripts/NewRogue/PlayerMove.cs
@@ -8,6 +8,7 @@ namespace Tea.NewRouge
 	public class PlayerMove : BaseCharacterController
 	{
 		public Transform a;
+		float virRotate;
 		public CinemachineVirtualCamera virCamera;
 		protected override void UpdateRotation()
 		{
@@ -18,25 +19,42 @@ namespace Tea.NewRouge
 		{
 			//base.HandleInput();
 
+			//moveDirection = new Vector3
+			//{
+			//	x = Input.GetAxisRaw("Horizontal"),
+			//	y = 0.0f,
+			//	z = Input.GetAxisRaw("Vertical")
+			//};
+			//Debug.Log($" {Mathf.Cos(0)} {Mathf.Cos(0.5f)} {Mathf.Cos(1)} \n {Mathf.Cos(virRotate)}");
 			moveDirection = new Vector3
 			{
-				x = Input.GetAxisRaw("Horizontal"),
+				x = Input.GetAxisRaw("Horizontal") * Mathf.Cos(virRotate * Mathf.Deg2Rad) +
+					Input.GetAxisRaw("Vertical") * Mathf.Sin(virRotate * Mathf.Deg2Rad),
 				y = 0.0f,
-				z = Input.GetAxisRaw("Vertical")
+				z = Input.GetAxisRaw("Horizontal") * -Mathf.Sin(virRotate * Mathf.Deg2Rad) +
+					Input.GetAxisRaw("Vertical") * Mathf.Cos(virRotate * Mathf.Deg2Rad)
 			};
+			var x = new Vector3();
+
 		}
 		public override void Update()
 		{
 			base.Update();
+
 			if (Input.GetKey(KeyCode.Q))
 			{
-				virCamera.transform.eulerAngles += new Vector3(0, 1, 0);
+				VirRotate(1);
 			}
 			if (Input.GetKey(KeyCode.E))
 			{
-				virCamera.transform.eulerAngles += new Vector3(0, -1, 0);
+				VirRotate(-1);
 			}
 
+		}
+		void VirRotate(float rotate)
+		{
+			virCamera.transform.eulerAngles += new Vector3(0, rotate, 0);
+			virRotate = virCamera.transform.eulerAngles.y;
 		}
 	}
 }
